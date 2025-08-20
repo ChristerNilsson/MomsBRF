82a4702f430ce6e02806e6b1e0b640ef1170f33d
Christer Nilsson <janchrister.nilsson@gmail.com>
2025-08-20 12:18:23 +0200

    x

--- sortera.py
+++ sortera.py
@@ -1,19 +1,13 @@
-import re
-from pathlib import Path
-import time
-
-# SIE_FIL = "202206"
-# SIE_FIL = "2022"
 SIE_FIL = "2023"
 
 preludium = []
 verifikationer = []
 
-path = Path(SIE_FIL + '.txt')
-text = path.read_text(encoding="utf8")
-for raw_line in text.splitlines():
+with open(SIE_FIL + '.txt', "r", encoding="utf-8") as f:
+	lines = f.readlines()
 
-	line = raw_line.strip()
+for line in lines:
+	line = line.strip()
 
 	if not line: continue
 	if line in ['{','}']: continue
@@ -21,10 +15,8 @@ for raw_line in text.splitlines():
 	if line.startswith("#VER"):
 		l = line.split(' ')
 		verifikationer.append({'sort':[l[3],l[2],l[1]], 'text': line, 'transaktioner': []})
-
 	elif line.startswith("#TRANS"):
 		verifikationer[-1]["transaktioner"].append(line)
-
 	else:
 		preludium.append(line)
 
